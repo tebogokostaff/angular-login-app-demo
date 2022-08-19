8c3a1ec5f8fbc8530d7d770c2f1da24693a37038
Tebogo Ramasike <tramasike@gmail.com>
2022-08-19 10:05:20 +0200

    chore: scaffold app structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    AdminComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
